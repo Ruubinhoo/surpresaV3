c9db8d987adbc3e851e6f18fb73100a5b966b42c
Rubens Junior <52179794+Ruubinhoo@users.noreply.github.com>
2023-09-17 00:53:53 -0300

    versao-Final-def
    Versão Final Definitiva

--- templates/static/java-script/script.js
+++ templates/static/java-script/script.js
@@ -1,6 +1,6 @@
 // Função para verificar e habilitar o botão com base na data e hora
 function verificarDataEHora() {
-    const dataAlvo = new Date('2023-10-06T00:00:00'); // Defina a data e hora alvo
+    const dataAlvo = new Date('2023-01-01T00:00:00'); // Defina a data e hora alvo
     const agora = new Date();
     const botoes = document.getElementsByClassName('btn');
 
